56b732c91c1ff1ef4159828a1ec0a05b2b34f8bf
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-23 11:31:03 -0300

    Finalizando

---
+++ src/Conteudo.tsx
@@ -0,0 +1,39 @@
+import logo from "./assets/logo-react.png";
+
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+        <p>Este é um exemplo de aplicação React.</p>
+
+        <article>
+          <h3>Artigo 1 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            labore autem sapiente veritatis numquam repudiandae! Quia sint illo
+            quo animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
+          </p>
+        </article>
+
+        <article>
+          <h3>Artigo 2 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            labore autem sapiente veritatis numquam repudiandae! Quia sint illo
+            quo animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
+          </p>
+        </article>
+
+        <article>
+          <h3>Artigo 3 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            labore autem sapiente veritatis numquam repudiandae! Quia sint illo
+            quo animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+}
