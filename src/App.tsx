3cf7ea921666a0c79e0530c3c55f644a42364c18
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-23 11:41:09 -0300

    Correção do exercício componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
+import Rodape from "./Rodape";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-
-      <main>
-        <section>
-          <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
-          <p>Este é um exemplo de aplicação React.</p>
-
-          <article>
-            <h3>Artigo 1 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reiciendis labore autem sapiente veritatis numquam repudiandae!
-              Quia sint illo quo animi ipsam. Ex dolore, facere culpa unde nihil
-              hic modi quis.
-            </p>
-          </article>
-
-          <article>
-            <h3>Artigo 2 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reiciendis labore autem sapiente veritatis numquam repudiandae!
-              Quia sint illo quo animi ipsam. Ex dolore, facere culpa unde nihil
-              hic modi quis.
-            </p>
-          </article>
-
-          <article>
-            <h3>Artigo 3 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reiciendis labore autem sapiente veritatis numquam repudiandae!
-              Quia sint illo quo animi ipsam. Ex dolore, facere culpa unde nihil
-              hic modi quis.
-            </p>
-          </article>
-        </section>
-      </main>
-
-      <footer>
-        <h2>Pequeno site criado com React</h2>
-        <p>
-          Desenvolvido por <b>João Pedro</b> &copy; 2025
-        </p>
-      </footer>
+      <Conteudo />
+      <Rodape />
     </>
   );
 }
