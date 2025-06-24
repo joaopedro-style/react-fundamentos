b7e4947f0cd54c91167ea59787de67542dae5f70
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-24 09:14:37 -0300

    Cria e reutiliza componente Artigo

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,12 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de exemplo</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        labore autem sapiente veritatis numquam repudiandae! Quia sint illo quo
+        animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
+      </p>
+    </article>
+  );
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,32 +7,11 @@ export default function Conteudo() {
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
-        <article>
-          <h3>Artigo 1 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-            labore autem sapiente veritatis numquam repudiandae! Quia sint illo
-            quo animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3>Artigo 2 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-            labore autem sapiente veritatis numquam repudiandae! Quia sint illo
-            quo animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3>Artigo 3 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-            labore autem sapiente veritatis numquam repudiandae! Quia sint illo
-            quo animi ipsam. Ex dolore, facere culpa unde nihil hic modi quis.
-          </p>
-        </article>
+        <Artigo />
       </section>
     </main>
   );
