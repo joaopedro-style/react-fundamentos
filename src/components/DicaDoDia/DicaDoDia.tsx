a3ff572a115480d2e0d344e321f3ae086475273e
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-25 10:42:32 -0300

    Adiciona classes para DicaDoDia

--- src/components/DicaDoDia/DicaDoDia.tsx
+++ src/components/DicaDoDia/DicaDoDia.tsx
@@ -3,6 +3,36 @@ import styled from "styled-components";
 const StyledDicaDoDia = styled.section`
   background-color: #bce8bc;
   border-left: 5px solid darkgreen;
+  padding: 1rem;
+  margin-top: 1rem;
+  border-radius: 4px;
+
+  h3 {
+    margin-top: 0;
+    color: darkgreen;
+  }
+
+  p {
+    color: green;
+    font-size: 1.1rem;
+  }
+
+  .site-oficial {
+    background-color: white;
+    text-align: center;
+    padding: 0.4rem;
+
+    a {
+      text-decoration: none;
+      color: darkgreen;
+      font-weight: bold;
+
+      &:hover,
+      &:focus {
+        text-decoration: underline;
+      }
+    }
+  }
 `;
 
 export default function DicaDoDia() {
@@ -14,7 +44,7 @@ export default function DicaDoDia() {
         Neste exemplo, estamos usando <b>styled-components</b>, uma famosa
         biblioteca para CSS In JS.
       </p>
-      <p>
+      <p className="site-oficial">
         Site official:{" "}
         <a href="https://styled-components.com/" target="_blank">
           Styled Components
