1a8edbdc403a9c31037479d9b4cbacb224a2652f
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-25 09:58:03 -0300

    Cria components DicaDoDia e ultiliza bliblioteca lib

--- src/components/DicaDoDia/DicaDoDia.tsx
+++ src/components/DicaDoDia/DicaDoDia.tsx
@@ -1,3 +1,25 @@
+import styled from "styled-components";
+
+const StyledDicaDoDia = styled.section`
+  background-color: #e8f5e9;
+  border-left: 5px solid darkgreen;
+`;
+
 export default function DicaDoDia() {
-  return <div>DicaDoDia</div>;
+  return (
+    <StyledDicaDoDia>
+      <h3>Dica do Dia</h3>
+      <p>
+        Você pode combinar diferentes formas de estilização em um projeto React.
+        Neste exemplo, estamos usando <b>styled-components</b>, uma famosa
+        biblioteca para CSS In JS.
+      </p>
+      <p>
+        Site official:{" "}
+        <a href="https://styled-components.com/" target="_blank">
+          Styled Components
+        </a>
+      </p>
+    </StyledDicaDoDia>
+  );
 }
