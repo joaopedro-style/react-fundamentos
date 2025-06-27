2be44347bf67faa395a6c14833e45e4ecc63fcc8
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-27 11:19:02 -0300

    Gera array de categorias sem valores repetidos

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -3,11 +3,14 @@ import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import estilos from "./Conteudo.module.css";
 import Saudacao from "./Saudacao";
 import ListaCursos from "../ListaCursos/ListaCursos";
+import cursos from "../../data/cursos";
 
 export default function Conteudo() {
+  const categorias = [...new Set(cursos.map((curso) => curso.categoria))];
+  console.log(categorias);
+
   return (
     <main className={estilos.conteudo}>
-      {/* Para combinar classes, use template string */}
       <section
         className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
       >
@@ -15,8 +18,6 @@ export default function Conteudo() {
         <Saudacao nome="João Pedro" classe="bg-emerald-100" />
 
         <p>Este é um exemplo de aplicação React.</p>
-
-        {/* Faça a lógica necessária para apresentar o componente Artigo e passar para ele os dados de cada curso proveniente de cursos. */}
 
         <ListaCursos />
       </section>
