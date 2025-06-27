4203b7b1e956464e9889b44e53bb3262a20a828d
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-27 11:00:27 -0300

    Aplica state de cor

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,15 +1,28 @@
 import estilos from "./Artigo.module.css";
 import type { Curso } from "../../../types/Curso";
+import { useState } from "react";
 
 type ArtigoProps = {
   dados: Curso;
 };
 
 export default function Artigo({ dados }: ArtigoProps) {
-  // Podemos receber a prop dados para depois desestruturar:
   const { titulo, preco, categoria } = dados;
+
+  const [cor, setCor] = useState("#f9f9f9");
+
+  const selecionar = () => {
+    setCor((cor) => {
+      return cor === "#f9f9f9" ? "#fc9803" : "#f9f9f9";
+    });
+  };
+
   return (
-    <article className={estilos.artigo}>
+    <article
+      onClick={selecionar}
+      className={estilos.artigo}
+      style={{ backgroundColor: cor }}
+    >
       <h3> {titulo} </h3>
       <p>
         <b>Categoria:</b> {categoria}
