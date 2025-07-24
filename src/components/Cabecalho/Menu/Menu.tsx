661d5605f9361827f9b7e85331a0666f6157bfbc
joaopedro-style <joaopedro18231@hotmail.com>
2025-07-24 09:53:50 -0300

    Cria breanch 13

--- src/components/Cabecalho/Menu/Menu.tsx
+++ src/components/Cabecalho/Menu/Menu.tsx
@@ -3,9 +3,9 @@ import estilos from "./Menu.module.css";
 export default function Menu() {
   return (
     <nav className={estilos.menu}>
-      <a href="">Home</a>
-      <a href="">Sobre</a>
-      <a href="">Contato</a>
+      <a href="/Home">Home</a>
+      <a href="/Sobre">Sobre</a>
+      <a href="/Contato">Contato</a>
     </nav>
   );
 }
---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,13 @@
+export default function Sobre() {
+  return (
+    <section>
+      <h2>Sobre nossa empresa</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
+        eligendi ex a qui molestiae dolore. Accusamus inventore dolores quasi
+        eaque quia! Recusandae excepturi reiciendis accusamus hic sunt impedit
+        libero est.
+      </p>
+    </section>
+  );
+}
