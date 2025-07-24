01e77ee4440aa9352847d0019038b9ce2aafe01b
joaopedro-style <joaopedro18231@hotmail.com>
2025-07-24 10:06:40 -0300

    Cria sobre contato e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
+import Contato from "./pages/Contato";
+import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <Conteudo />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+
       <Rodape />
     </>
   );
